3685889a575ceab059384b8268dab3b6ffd2b431
minchulcho <32747972+minchulcho@users.noreply.github.com>
2021-03-23 11:01:34 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    let bodySelector = document.querySelector('body');
+    //bodySelector.innerHTML = ""; 
+    //bodySelector.style.backgroundColor = 'red';
+    let chil =  bodySelector.children; 
+    // chil[0].style.color = 'blue';
+    console.log( chil.length )
+})
